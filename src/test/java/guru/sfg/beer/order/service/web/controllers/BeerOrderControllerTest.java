89d64e37235758313fdea6fb2dd9aeb39b324179
Michael Ortiz <12751082+Odysseuss@users.noreply.github.com>
2023-03-18 11:41:19 -0400

    integration test for new order to allocate

---
+++ src/test/java/guru/sfg/beer/order/service/web/controllers/BeerOrderControllerTest.java
@@ -0,0 +1,24 @@
+package guru.sfg.beer.order.service.web.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeerOrderControllerTest {
+
+    @Test
+    void listOrders() {
+    }
+
+    @Test
+    void placeOrder() {
+    }
+
+    @Test
+    void getOrder() {
+    }
+
+    @Test
+    void pickupOrder() {
+    }
+}
